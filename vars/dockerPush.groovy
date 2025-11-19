5c116cc0765639c73d4d7a0bd29c0c79f6575d4f
Umerfarooqsyed <umerfarooqsyed18@gmail.com>
2025-11-19 11:05:02 +0500

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String ImageTag, String credsId){
+def call(String ImageName, String ImageTag, String credsId){
     withCredentials([usernamePassword(credentialsId: credsId, usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh "docker login -u ${USER} -p ${PASS}"
-        sh "docker push ${USER}/${Project}:${ImageTag}"
+        sh "docker push ${ImageName}:${ImageTag}"
     }
 }
