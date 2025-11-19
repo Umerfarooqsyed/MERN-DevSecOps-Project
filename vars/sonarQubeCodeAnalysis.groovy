ca8301ef24a9d676f27c9ff4988b8fb3e37728c1
Umerfarooqsyed <umerfarooqsyed18@gmail.com>
2025-11-19 10:10:20 +0500

    added with credentials instead of normal variable login

--- vars/sonarQubeCodeAnalysis.groovy
+++ vars/sonarQubeCodeAnalysis.groovy
@@ -1,84 +1,87 @@
-def call(String language, String projectName, String projectKey, String sonarUrl, String sonarToken) {
+def call(String language, String projectName, String projectKey, String sonarUrl) {
 
-    withSonarQubeEnv("MySonarServer") {
+    withCredentials([string(credentialsId: 'sonar-token', variable: 'SONAR_TOKEN')]) {
 
-        // ---------- MAVEN (Java / Spring Boot) ----------
-        if (language == "maven") {
-            sh """
-                mvn sonar:sonar \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${sonarToken}
-            """
-        }
+        withSonarQubeEnv("MySonarServer") {
 
-        // ---------- NODE.JS / MERN STACK ----------
-        else if (language == "nodejs") {
-            sh """
-                sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.sources=. \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${sonarToken}
-            """
-        }
+            // ---------- MAVEN (Java / Spring Boot) ----------
+            if (language == "maven") {
+                sh """
+                    mvn sonar:sonar \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.host.url=${sonarUrl} \
+                    -Dsonar.login=$SONAR_TOKEN
+                """
+            }
 
-        // ---------- PYTHON ----------
-        else if (language == "python") {
-            sh """
-                sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.sources=. \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${sonarToken}
-            """
-        }
+            // ---------- NODE.JS / MERN STACK ----------
+            else if (language == "nodejs") {
+                sh """
+                    sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=${sonarUrl} \
+                    -Dsonar.login=$SONAR_TOKEN
+                """
+            }
 
-        // ---------- GO / GOLANG ----------
-        else if (language == "go") {
-            sh """
-                sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.sources=. \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${sonarToken}
-            """
-        }
+            // ---------- PYTHON ----------
+            else if (language == "python") {
+                sh """
+                    sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=${sonarUrl} \
+                    -Dsonar.login=$SONAR_TOKEN
+                """
+            }
 
-        // ---------- GRADLE (Kotlin, Java) ----------
-        else if (language == "gradle") {
-            sh """
-                gradle sonarqube \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.host.url=${sonarUrl} \
-                -Dsonar.login=${sonarToken}
-            """
-        }
+            // ---------- GO / GOLANG ----------
+            else if (language == "go") {
+                sh """
+                    sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=${sonarUrl} \
+                    -Dsonar.login=$SONAR_TOKEN
+                """
+            }
 
-        // ---------- DOTNET ----------
-        else if (language == "dotnet") {
-            sh """
-                SonarScanner.MSBuild.exe begin \
-                /k:${projectKey} \
-                /n:${projectName} \
-                /d:sonar.host.url=${sonarUrl} \
-                /d:sonar.login=${sonarToken}
+            // ---------- GRADLE (Kotlin, Java) ----------
+            else if (language == "gradle") {
+                sh """
+                    gradle sonarqube \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.projectName=${projectName} \
+                    -Dsonar.host.url=${sonarUrl} \
+                    -Dsonar.login=$SONAR_TOKEN
+                """
+            }
 
-                msbuild
+            // ---------- DOTNET ----------
+            else if (language == "dotnet") {
+                sh """
+                    SonarScanner.MSBuild.exe begin \
+                    /k:${projectKey} \
+                    /n:${projectName} \
+                    /d:sonar.host.url=${sonarUrl} \
+                    /d:sonar.login=$SONAR_TOKEN
 
-                SonarScanner.MSBuild.exe end \
-                /d:sonar.login=${sonarToken}
-            """
-        }
+                    msbuild
 
-        // ---------- Unsupported ----------
-        else {
-            error "Unsupported language type: ${language}"
+                    SonarScanner.MSBuild.exe end \
+                    /d:sonar.login=$SONAR_TOKEN
+                """
+            }
+
+            // ---------- Unsupported ----------
+            else {
+                error "Unsupported language type: ${language}"
+            }
         }
     }
 }
