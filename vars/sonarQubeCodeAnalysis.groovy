c04c80bea7473c0ca33778f198c39d6662b65758
Umerfarooqsyed <umerfarooqsyed18@gmail.com>
2025-12-02 10:36:24 +0500

    Update sonarQubeCodeAnalysis.groovy

--- vars/sonarQubeCodeAnalysis.groovy
+++ vars/sonarQubeCodeAnalysis.groovy
@@ -6,76 +6,48 @@ def call(String language, String projectName, String projectKey, String sonarUrl
 
             // ---------- MAVEN (Java / Spring Boot) ----------
             if (language == "maven") {
-                sh """
-                    mvn sonar:sonar \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.projectName=${projectName} \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh 'mvn sonar:sonar -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.host.url=$sonarUrl -Dsonar.login=$SONAR_TOKEN'
+                }
             }
 
             // ---------- NODE.JS / MERN STACK ----------
             else if (language == "nodejs") {
-                sh """
-                    sonar-scanner \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.projectName=${projectName} \
-                    -Dsonar.sources=. \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh 'sonar-scanner -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.sources=. -Dsonar.host.url=$sonarUrl -Dsonar.login=$SONAR_TOKEN'
+                }
             }
 
             // ---------- PYTHON ----------
             else if (language == "python") {
-                sh """
-                    sonar-scanner \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.projectName=${projectName} \
-                    -Dsonar.sources=. \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh 'sonar-scanner -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.sources=. -Dsonar.host.url=$sonarUrl -Dsonar.login=$SONAR_TOKEN'
+                }
             }
 
             // ---------- GO / GOLANG ----------
             else if (language == "go") {
-                sh """
-                    sonar-scanner \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.projectName=${projectName} \
-                    -Dsonar.sources=. \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh 'sonar-scanner -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.sources=. -Dsonar.host.url=$sonarUrl -Dsonar.login=$SONAR_TOKEN'
+                }
             }
 
             // ---------- GRADLE (Kotlin, Java) ----------
             else if (language == "gradle") {
-                sh """
-                    gradle sonarqube \
-                    -Dsonar.projectKey=${projectKey} \
-                    -Dsonar.projectName=${projectName} \
-                    -Dsonar.host.url=${sonarUrl} \
-                    -Dsonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh 'gradle sonarqube -Dsonar.projectKey=$projectKey -Dsonar.projectName=$projectName -Dsonar.host.url=$sonarUrl -Dsonar.login=$SONAR_TOKEN'
+                }
             }
 
             // ---------- DOTNET ----------
             else if (language == "dotnet") {
-                sh """
-                    SonarScanner.MSBuild.exe begin \
-                    /k:${projectKey} \
-                    /n:${projectName} \
-                    /d:sonar.host.url=${sonarUrl} \
-                    /d:sonar.login=$SONAR_TOKEN
-
-                    msbuild
-
-                    SonarScanner.MSBuild.exe end \
-                    /d:sonar.login=$SONAR_TOKEN
-                """
+                withEnv(["SONAR_TOKEN=${SONAR_TOKEN}"]) {
+                    sh '''
+                        SonarScanner.MSBuild.exe begin /k:$projectKey /n:$projectName /d:sonar.host.url=$sonarUrl /d:sonar.login=$SONAR_TOKEN
+                        msbuild
+                        SonarScanner.MSBuild.exe end /d:sonar.login=$SONAR_TOKEN
+                    '''
+                }
             }
 
             // ---------- Unsupported ----------
