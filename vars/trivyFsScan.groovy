8e65f626533d5fea3afa7000ca98d4c3c439dd26
Umerfarooqsyed <umerfarooqsyed18@gmail.com>
2025-12-02 10:53:47 +0500

    Update trivyFsScan.groovy

--- vars/trivyFsScan.groovy
+++ vars/trivyFsScan.groovy
@@ -1,17 +1,3 @@
-def call(String path=".", String reportPath="trivy-fs-report.json", String severity="CRITICAL,HIGH") {
-    echo "Starting Trivy File System scan..."
-    echo "Scan Path: ${path}"
-    echo "Report File: ${reportPath}"
-    echo "Pipeline will fail if Severity is ${severity}"
-
-    // Run Trivy FS scan
-    sh """
-        trivy fs ${path} \
-        --exit-code 1 \
-        --severity ${severity} \
-        -o ${reportPath} \
-        --format json
-    """
-
-    echo "Trivy FS scan completed. Report saved at: ${reportPath}"
+def call(){
+  sh "trivy fs ."
 }
